8a7a1794778276a89161084538dce7881fff4074
LauraWaldron <lwaldron2021@fau.edu>
2023-11-20 11:10:04 -0500

    edited the collisions and ball files

--- core/src/com/pong/game/CollisionUtils.java
+++ core/src/com/pong/game/CollisionUtils.java
@@ -1,5 +1,6 @@
 package com.pong.game;
 
+import com.badlogic.gdx.math.Circle;
 //libGDX Intersector class
 import com.badlogic.gdx.math.Intersector; //use intersector class for collision detection
 
@@ -8,7 +9,13 @@ public class CollisionUtils {
         // Add logic for detecting collisions between the paddle and the ball
         //return false;
         //according to Chatgpt can use intersector to check this
-        return Intersector.overlaps(paddle.getBoundingBox(), ball.getBoundingCircle());
+        // Convert Rectangle to Circle for collision check
+        Circle paddleCircle = new Circle(paddle.getBoundingBox().getX() + paddle.getBoundingBox().getWidth() / 2,
+        paddle.getBoundingBox().getY() + paddle.getBoundingBox().getHeight() / 2,
+        Math.max(paddle.getBoundingBox().getWidth(), paddle.getBoundingBox().getHeight()) / 2);
+
+        // Check for collision between the converted Circle and the Ball's Circle
+        return Intersector.overlaps(paddleCircle, ball.getBoundingCircle());
     }
 
     public static void handleCollision(Paddle paddle, Ball ball) {
